234650b72837c7f4da92a13b87ace425161d7e70
Suraj Khosla <surajkhosla79@gmail.com>
2022-04-13 12:40:41 -0500

    added a bunch of stuff and broke everything

--- index.ts
+++
@@ -1,9 +0,0 @@
-const { Client, Intents}  = require('discord.js')
-const { TOKEN } = require('./config.json')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
-
-client.once('ready', () => {
-    console.log('online')
-})
-
-client.login(TOKEN)
